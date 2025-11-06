1b1794158e5eaac7e4f8d4ffb268d23359f0da37
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-11-06 19:16:27 +0530

    Add MongoDB connection utility in server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const connectDB = async (mongoURI) => {
+try {
+await mongoose.connect(mongoURI);
+console.log('MongoDB connected');
+} catch (err) {
+console.error(err.message);
+process.exit(1);
+}
+};
+module.exports = connectDB;
